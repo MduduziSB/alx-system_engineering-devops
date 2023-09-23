7fec99ee55722de841f6557f43947e8283bec4b4
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-09-22 20:12:02 -0700

    Task2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+#The code creates a manifest that kills a process named killmenow
+exec { 'kill_killmenow_process':
+  command     => 'pkill killmenow',
+  onlyif      => 'pgrep killmenow',
+  refreshonly => true,
+}
