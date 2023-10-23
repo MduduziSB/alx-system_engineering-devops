cf5611d5bf403595fd4e3b0675436c0579eac8f5
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-10-23 10:43:17 -0700

    Modified Task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,18 +12,15 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com"
     user_response = requests.get(f"{user_url}/users/{employee_id}")
-    name = user_response.json().get("name")
+    name = user_response.json().get("username")
     tasks_response = requests.get(f"{user_url}/todos?userId={employee_id}")
     tasks_data = tasks_response.json()
-
-    # Count completed tasks and total tasks
-    completed_tasks = [task for task in tasks_data if task["completed"]]
-    completed = len(completed_tasks)
 
     # Export data to CSV
     csv_filename = f"{employee_id}.csv"
     with open(csv_filename, mode='w') as csv_file:
-        csv_writer = csv.writer(csv_file)
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL, lineterminator='\n')
 
         for task in tasks_data:
             csv_writer.writerow([employee_id,
