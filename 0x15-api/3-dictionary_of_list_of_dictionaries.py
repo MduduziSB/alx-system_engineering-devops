c0e4cee8509ef5f98e021fe1186e768a9c48e260
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-10-23 11:55:27 -0700

    Task3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This script exports data in the json format"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    user_url = requests.get("https://jsonplaceholder.typicode.com/users")
+    user_url = user_url.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+
+    todo_dic = {}
+    task_list = []
+
+    for idx in user_url:
+        for task in todos:
+            if task.get('userId') == idx.get('id'):
+                json_data = {
+                             "username": idx.get('username'),
+                             "task": task['title'],
+                             "completed": task['completed']}
+                task_list.append(json_data)
+        todo_dic[idx.get('id')] = task_list
+
+    filename = "todo_all_employees.json"
+    with open(filename, mode='w') as File:
+        json.dump(todo_dic, File)
