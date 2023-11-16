6ec5a8a647bce1d3642f9a25eb78e603f51b886a
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-11-16 06:56:54 -0800

    Mandatory task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# update the limit nginx.
+exec { 'fix-limits':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/bin',
+}
+
+exec { 'nginx_restart':
+  command     => '/etc/init.d/nginx restart',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/nginx.conf'],
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+ALX fourth project on web stack debugging (0x1B. Web stack debugging #4)
