e89c6584e0d04a45df9d79096e433e756ca5a891
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-12-15 23:52:46 -0800

    Modified Task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # 0-strace_is_your_friend.pp
 
-file { '/var/www/html/wp-settings.php':
-  ensure  => 'file',
-  content => template('your_module/wp-settings.php.erb'),
-  replace => true,
+exec { 'replace phpp with php':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
